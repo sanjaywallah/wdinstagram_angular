0b845d6dda9ff10bc0785ef748b6298a4d4a2f27
Sanjay Persaud <sanjaywallah@gmail.com>
2017-08-15 21:48:42 -0400

    added views and controllers

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular.module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+      .state("entryIndex", {
+        url: "/entry",
+        templateUrl: "js/entry/index.html",
+        controller: "EntryIndexController",
+        controllerAs: "vm"
+      })
+      .state("entryShow", {
+        url: "entry/:id",
+        templateUrl: "js/entry/show.html",
+        controller: "EntryShowController",
+        controllerAs: "vm"
+      })
+  }
 })();
---
+++ js/index.controller.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function(){
+  angular
+    .module("wdinstagram")
+    .controller("EntryIndexController", EntryIndexControllerFunction)
+
+    function EntryIndexControllerFunction(){
+      
+    }
+})

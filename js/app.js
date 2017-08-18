0f1e09f58ae1454c68ab5f5190089127f9a48f7b
Sanjay Persaud <sanjaywallah@gmail.com>
2017-08-18 07:08:10 -0400

    basic show and index

--- js/app.js
+++ js/app.js
@@ -1,28 +1,67 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [
-    "ui.router",
-    "ngResource"
-  ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ])
-
-  function RouterFunction($stateProvider){
-    $stateProvider
-      .state("entryIndex", {
-        url: "/entry",
-        templateUrl: "js/entry/index.html",
-        controller: "EntryIndexController",
-        controllerAs: "vm"
-      })
-      .state("entryShow", {
-        url: "entry/:id",
-        templateUrl: "js/entry/show.html",
-        controller: "EntryShowController",
-        controllerAs: "vm"
-      })
-  }
+  angular.module("wdinstagram",
+  [])
 })();
+
+(function () {
+  angular
+    .module('wdinstagram', [
+      'ui.router',
+      'ngResource'
+    ])
+    .config([
+      '$stateProvider',
+      RouterFunction
+    ])
+
+    .controller('EntryIndexController', [
+      'Entry',
+      EntryIndexControllerFunction
+    ])
+
+    .controller('EntryShowController', [
+      'Entry',
+      '$stateParams',
+      EntryShowControllerFunction
+    ])
+
+    .factory('Entry', [
+      '$resource',
+      entryService
+    ])
+
+    function entryService ($resource) {
+      return $resource('http://localhost:3000/entries/:id', {}, {
+        update: {
+          method: 'PUT'
+        }
+      })
+    }
+
+    function RouterFunction ($stateProvider) {
+      $stateProvider
+      .state('entryIndex', {
+        url: '/entries',
+        templateUrl: 'js/ng-views/index.html',
+        controller: 'EntryIndexController',
+        controllerAs: 'vm'
+      })
+      .state('entryShow', {
+        url: '/entries/:id',
+        templateUrl: 'js/ng-views/show.html',
+        controller: 'EntryShowController',
+        controllerAs: 'vm'
+      })
+    }
+
+ function EntryIndexControllerFunction (Entry) {
+   this.entries = Entry.query()
+ }
+
+ function EntryShowControllerFunction (Entry, $stateParams) {
+   this.entry = Entry.get({id: $stateParams.id})
+ }
+
+})()
